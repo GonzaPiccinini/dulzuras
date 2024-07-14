3edee9a03e1076a7884428751e76aa0c61dfadbc
Gonnzalo Piccinini <gonzipiccinini@gmail.com>
2024-07-14 20:41:08 -0300

    Added firebase with config

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -2,18 +2,10 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 interface Props {
   children: JSX.Element | React.ReactNode
-}
-
-export const AppStyles = [
-  "font-Montserrat",
-  "min-h-screen",
-  "flex",
-  "justify-center",
-  "items-center"
-]
-
+}              
+                           
 export const AppContainer = ({ 
-  children 
+  children                      
 }: Props) => {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
